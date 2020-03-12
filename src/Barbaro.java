5b6da161c7a57fe2a98108cdc1cee0ffe0cbdade
leormoreira <61908514+leormoreira@users.noreply.github.com>
2020-03-12 21:55:26 -0200

    Add files via upload

---
+++ src/Barbaro.java
@@ -0,0 +1,18 @@
+package tema1;
+
+public class Barbaro {
+    String nome;
+    double vida;
+    double ataque;
+    double defesa;
+
+    Barbaro (String nome, double vida, double ataque, double defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+    }
+
+
+
+}
